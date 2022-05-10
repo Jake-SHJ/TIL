ec9e5a713e8e6289aaffde7d96e5b3feb88e8361
jake <guswns1853@gmail.com>
2022-05-10 22:52:13 +0900

    fix: 1.1 초난감 DAO 누락된 파일 추가

---
+++ springbook/src/SpringbookApplication.java
@@ -0,0 +1,25 @@
+import java.sql.SQLException;
+import user.dao.UserDao;
+import user.domain.User;
+
+public class SpringbookApplication {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("test2");
+        user.setName("jake");
+        user.setPassword("test");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
