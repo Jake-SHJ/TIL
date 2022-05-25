04b2ed80d90e6012b917039d95476fb4f5b4b6cc
jake <guswns1853@gmail.com>
2022-05-25 21:36:58 +0900

    feat: 3.5.3 템플릿/콜백의 응용
    - 중복의 제거와 템플릿/콜백 설계

--- springbook/src/learningtest/template/CalcSumTest.java
+++ springbook/src/learningtest/template/CalcSumTest.java
@@ -3,13 +3,26 @@ package learningtest.template;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import java.io.IOException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcSumTest {
+
+    Calculator calculator;
+    String numFilePath;
+
+    @Before public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilePath = getClass().getResource("numbers.txt").getPath();
+    }
+
     @Test
     public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
-        assertThat(sum, is(10));
+        assertThat(calculator.calcSum(this.numFilePath), is(10));
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        assertThat(calculator.calcMultiply(this.numFilePath), is(24));
     }
 }
