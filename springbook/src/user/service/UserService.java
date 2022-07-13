479765e70b1f6df96586de93229fd116c5c0bea5
jake <guswns1853@gmail.com>
2022-07-13 21:43:07 +0900

    feat: 5.4.1 JavaMail을 이용한 메일 발송 기능
    - 발송 메서드 구현(호출 메서드는 주석 처리)

--- springbook/src/user/service/UserService.java
+++ springbook/src/user/service/UserService.java
@@ -1,6 +1,15 @@
 package user.service;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Properties;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -20,8 +29,7 @@ public class UserService {
 
     private PlatformTransactionManager transactionManager;
 
-    public void setTransactionManager(
-        PlatformTransactionManager transactionManager) {
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
         this.transactionManager = transactionManager;
     }
 
@@ -60,6 +68,27 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+//        sendUpgradeEmail(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.test.com");
+        Session s = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(s);
+        try {
+            message.setFrom(new InternetAddress("useradmin@test.com"));
+            message.addRecipient(RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드 되었습니다.");
+
+            Transport.send(message);
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void add(User user) {
