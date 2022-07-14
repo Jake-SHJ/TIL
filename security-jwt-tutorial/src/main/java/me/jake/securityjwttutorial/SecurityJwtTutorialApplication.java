d20a378fbbe933c54bd59778858480d346df4d55
Jake-SHJ <guswns1853@gmail.com>
2022-07-14 21:01:54 +0900

    feat: Security-JWT-Tutorial 프로젝트 생성
    - 컨트롤러 생성 및 401 Unauthorized 에러 확인

---
+++ security-jwt-tutorial/src/main/java/me/jake/securityjwttutorial/SecurityJwtTutorialApplication.java
@@ -0,0 +1,13 @@
+package me.jake.securityjwttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtTutorialApplication.class, args);
+	}
+
+}
