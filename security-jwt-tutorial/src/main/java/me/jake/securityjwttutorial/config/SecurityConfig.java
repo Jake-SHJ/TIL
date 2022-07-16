de00d37e7b2e2680e4d34ee3a16e1ebb5bba6ff0
Jake-SHJ <guswns1853@gmail.com>
2022-07-16 13:51:31 +0900

    feat: SecurityConfig 에 JWT 설정 추가
    - JWT 관련 모든 코드들을 SecurityConfig 에 적용

--- security-jwt-tutorial/src/main/java/me/jake/securityjwttutorial/config/SecurityConfig.java
+++ security-jwt-tutorial/src/main/java/me/jake/securityjwttutorial/config/SecurityConfig.java
@@ -1,12 +1,41 @@
 package me.jake.securityjwttutorial.config;
 
+import me.jake.securityjwttutorial.jwt.JwtAccessDeniedHandler;
+import me.jake.securityjwttutorial.jwt.JwtAuthenticationEntryPoint;
+import me.jake.securityjwttutorial.jwt.JwtSecurityConfig;
+import me.jake.securityjwttutorial.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메서드 단위로 사용하기 위함
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+        TokenProvider tokenProvider,
+        JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+        JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -18,8 +47,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().disable()
+
+            .exceptionHandling()
+            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .accessDeniedHandler(jwtAccessDeniedHandler)
+
+            .and()
+            .headers()
+            .frameOptions()
+            .sameOrigin()
+
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
             .authorizeRequests()
             .antMatchers("/api/hello").permitAll()
-            .anyRequest().authenticated();
+            .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/signup").permitAll()
+            .anyRequest().authenticated()
+
+            .and()
+            .apply(new JwtSecurityConfig(tokenProvider));
     }
 }
--- security-jwt-tutorial/src/main/java/me/jake/securityjwttutorial/jwt/JwtAuthenticationEntryPoint.java
+++ security-jwt-tutorial/src/main/java/me/jake/securityjwttutorial/jwt/JwtAuthenticationEntryPoint.java
@@ -6,7 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     // 유효한 자격 증명을 제공하지 않고 접근하려 할때 401 Unauthorized 에러 반환
     @Override
