71da1d066f104e00f4e7b5d4c4975579420ace78
Jake-SHJ <guswns1853@gmail.com>
2022-07-16 13:39:45 +0900

    feat: 에러 핸들러 class 생성
    - AuthenticationEntryPoint 를 이용한 401 Unauthorized 에러 핸들링
    - AccessDeniedHandler 를 이용한 403 Forbidden 에러 핸들링

---
+++ security-jwt-tutorial/src/main/java/me/jake/securityjwttutorial/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+package me.jake.securityjwttutorial.jwt;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    // 유효한 자격 증명을 제공하지 않고 접근하려 할때 401 Unauthorized 에러 반환
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
